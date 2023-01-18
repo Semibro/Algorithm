5e4178e7ec6af8044157c61fe8e980b162533eb9
김준형 (JunHyung Kim) <wnsgud6232@naver.com>
2023-01-18 21:46:55 +0900

    [Bronze I] Title: 이항 계수 1, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
@@ -0,0 +1,12 @@
+N, K = map(int, input().split())
+
+def factorial(n):
+  ans = 1
+  for i in range(2, n+1):
+    ans *= i
+  return ans
+
+def bino_coef_factorial(n, k):
+  return factorial(n) // factorial(k) // factorial(n-k)
+
+print(bino_coef_factorial(N, K))
