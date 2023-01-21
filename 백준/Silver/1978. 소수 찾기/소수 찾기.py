aec356288cb02042f199875e9450a5cc030a2ee7
김준형 (JunHyung Kim) <wnsgud6232@naver.com>
2023-01-21 22:06:12 +0900

    [Silver V] Title: 소수 찾기, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/1978. 소수 찾기/소수 찾기.py
+++ 백준/Silver/1978. 소수 찾기/소수 찾기.py
@@ -2,6 +2,7 @@ n = int(input())
 number_list = list(map(int, input().split()))
 count = 0
 
+# 1이 들어왔을 때는 pass하고 그 외의 숫자에 대한 소수 판별
 def is_prime_number(number):
   if number == 1:
     pass
@@ -11,6 +12,7 @@ def is_prime_number(number):
         return False
     return True
 
+# if문의 True값을 통해 소수면 count에 +1
 for i in number_list:
   if is_prime_number(i):
     count += 1
