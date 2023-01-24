04e98ebda2175f271fafaf5f78d9b2789c6424b5
김준형 (JunHyung Kim) <wnsgud6232@naver.com>
2023-01-24 23:01:39 +0900

    Update 수 정렬하기 2.py

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
@@ -1,11 +1,12 @@
 import sys
+n = int(sys.stdin.readline()) # 시간초과 이슈로 인한 sys 사용용
+bin_list = []              # 정렬에 사용할 빈 리스트 생성
 
-N = int(sys.stdin.readline())
-arr = []
+for i in range(n):
+  number = int(sys.stdin.readline())
+  bin_list.append(number)  # for문을 활용해 숫자를 입력받고 빈 리스트에 저장
 
-for i in range(N):
-    arr.append(int(sys.stdin.readline()))
+bin_list.sort()            # sort를 빈 리스트를 오름차순 정렬렬
 
-arr.sort()
-for i in range(N):
-    print(arr[i])
+for i in range(n):
+  print(bin_list[i])       # for문을 활용해 출력
