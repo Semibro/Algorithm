19902d3076be17073f5ba8a4c40aa138c30a5f07
김준형 (JunHyung Kim) <wnsgud6232@naver.com>
2023-01-24 22:54:06 +0900

    [Bronze III] Title: 오븐 시계, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.py
@@ -0,0 +1,15 @@
+hour, minute = map(int, input().split())
+timer = int(input())
+
+hour += timer // 60   # 시간은 60분으로 나눈 몫이 더해진다.
+minute += timer % 60  # 분은 60분으로 나눈 나머지가 더해진다.
+
+if minute >= 60:  # 만약 60분으로 나눈 나머지가 더해진 후의 값이 60분 보다 많다면
+                  # 즉, 1시간이 추가된다면면
+  hour += 1       # 1시간 추가
+  minute -= 60    # 60분을 빼서 나머지 분 저장
+
+if hour >= 24:    # 24시간이 되면 0으로 바꿔줘야 하므로 조건을 옆에와 같이 걸고
+  hour -= 24      # 시간에서 24을 빼서 0으로 바뀔 수 있게 저장
+
+print(hour, minute)
