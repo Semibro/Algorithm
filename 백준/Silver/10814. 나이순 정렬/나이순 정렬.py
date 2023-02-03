9869fcafeb6433fab66b161317e1e18becfa9b57
김준형 (JunHyung Kim) <wnsgud6232@naver.com>
2023-02-03 19:18:41 +0900

    [Silver V] Title: 나이순 정렬, Time: 4208 ms, Memory: 46276 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -1,12 +1,9 @@
 n = int(input())
-bin_list = []
-
-for i in range(n):
-  age, name = input().split()
-  age = int(age)
-  bin_list.append((age, name))
-
-bin_list.sort(key = lambda x : x[0])
-
-for j in bin_list:
-  print(j[0], j[1])
+mem_lst = [] # 멤버를 저장할 리스트 생성
+for _ in range(n):
+    age, name = input().split() # 나이와 이름 input으로 받음
+    age = int(age) # 나이를 int형으로 변경
+    mem_lst.append((age, name)) # 리스트에 저장
+mem_lst.sort(key = lambda x: x[0]) # 람다식을 사용해서 나이순으로 정렬
+for i in mem_lst:
+    print(i[0], i[1])
