92e92f4d3c9f62b53051fd70415574398fefb2c8
김준형 (JunHyung Kim) <wnsgud6232@naver.com>
2023-02-19 22:42:04 +0900

    Update Hashing.py

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -2,5 +2,5 @@ n = int(input())
 alpha = input()
 result = 0
 for i in range(n):
-    result += ((ord(alpha[i])-96) * (31**i)) % 1234567891
-print(result)
+    result += (ord(alpha[i])-96) * (31**i)
+print(result % 1234567891)
