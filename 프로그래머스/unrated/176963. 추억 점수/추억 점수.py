916e1b27b43e3c29c8f4082d3699b596d367c605
김준형 (JunHyung Kim) <wnsgud6232@naver.com>
2023-04-06 13:39:58 +0900

    [unrated] Title: 추억 점수, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.py
@@ -0,0 +1,11 @@
+def solution(name, yearning, photo):
+    answer = []
+    for i in photo:
+        res = 0
+        for j in i:
+            if j in name:
+                res += yearning[name.index(j)]
+            else:
+                pass
+        answer.append(res)
+    return answer
