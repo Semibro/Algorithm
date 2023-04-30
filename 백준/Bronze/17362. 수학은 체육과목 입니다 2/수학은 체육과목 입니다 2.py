37905225b2a4c0f7e4abad8780c02f94a2aa70a9
김준형 (JunHyung Kim) <wnsgud6232@naver.com>
2023-04-30 15:08:04 +0900

    [Bronze IV] Title: 수학은 체육과목 입니다 2, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/17362. 수학은 체육과목 입니다 2/수학은 체육과목 입니다 2.py
@@ -0,0 +1,12 @@
+n = int(input())
+result = n % 8
+if result == 1:
+    print(1)
+elif result == 2 or result == 0:
+    print(2)
+elif result == 3 or result == 7:
+    print(3)
+elif result == 4 or result == 6:
+    print(4)
+else:
+    print(5)
