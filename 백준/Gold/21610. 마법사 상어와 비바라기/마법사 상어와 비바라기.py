73a96d0273b703cd39078657402f84c5279322d1
김준형 (JunHyung Kim) <wnsgud6232@naver.com>
2023-06-27 16:51:38 +0900

    [Gold V] Title: 마법사 상어와 비바라기, Time: 496 ms, Memory: 116624 KB -BaekjoonHub

---
+++ 백준/Gold/21610. 마법사 상어와 비바라기/마법사 상어와 비바라기.py
@@ -0,0 +1,40 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+board = [list(map(int, input().split())) for _ in range(N)]
+delta = [(0, 0), (0, -1), (-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1)]
+default = [(N-1, 0), (N-1, 1), (N-2, 0), (N-2, 1)]
+for _ in range(M):
+    d, s = map(int, input().split())
+    cloud = []
+
+    # [1] 구름이동
+    for cx, cy in default:
+        dx, dy = (cx+delta[d][0]*s+N)%N, (cy+delta[d][1]*s+N)%N
+        cloud.append((dx, dy))
+        # [2] 물의 양 증가
+        board[dx][dy] += 1
+        # [3] 모든 구름이 사라진다
+
+    # [4] 물복사버그
+    for tx, ty in cloud:
+        for ax, ay in ((1, 1), (-1, -1), (1, -1), (-1, 1)):
+            sx, sy = tx+ax, ty+ay
+            if 0<=sx<N and 0<=sy<N and board[sx][sy] > 0:
+                board[tx][ty] += 1
+
+    # [5] 구름이 있던 곳을 제외하고, 물의 양이 2이상인 칸에 모두 -2
+    default = []
+    for x in range(N):
+        for y in range(N):
+            if board[x][y] >= 2 and (x, y) not in cloud:
+                board[x][y] -= 2
+                default.append((x, y))
+
+answer = 0
+for i in range(N):
+    for j in range(N):
+        answer += board[i][j]
+
+print(answer)
