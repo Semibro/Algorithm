1accf06fa1aac85f823e6f140a460df27015c9b3
김준형 (JunHyung Kim) <wnsgud6232@naver.com>
2023-06-27 15:55:20 +0900

    [Gold IV] Title: 뱀, Time: 160 ms, Memory: 116648 KB -BaekjoonHub

--- 백준/Gold/3190. 뱀/뱀.py
+++ 백준/Gold/3190. 뱀/뱀.py
@@ -3,14 +3,15 @@ input = sys.stdin.readline
 from collections import deque
 
 N = int(input())
-apple = []
+board = [[9]+[0]*N+[9] for _ in range(N)]
+board = [[9]*(N+2)] + board + [[9]*(N+2)]
 for _ in range(int(input())):
     x, y = map(int, input().split())
-    apple.append((x, y))
-move = [0] * 10001
+    board[x][y] = 1
+move = ['.'] * 10001
 for _ in range(int(input())):
-    a, b = input().split()
-    move[int(a)] = b
+    X, D = input().split()
+    move[int(X)] = D
 
 # 뱀의 꼬리가 줄어드므로 덱을 이용해 풀이 진행
 snake = deque()
@@ -25,10 +26,10 @@ while True:
     answer += 1
     x, y = snake[0][0], snake[0][1]  # 현재 머리
     dx, dy = x+delta[dist][0], y+delta[dist][1]  # 이동한 머리
-    if 1<=dx<=N and 1<=dy<=N and (dx, dy) not in snake:  # 벽이거나 뱀이 아닐 때
+    if board[dx][dy] != 9 and (dx, dy) not in snake:  # 벽이거나 뱀이 아닐 때
         snake.appendleft((dx, dy))  # 머리 위치 확장
-        if (dx, dy) in apple:  # 사과일 때
-            apple.remove((dx, dy))   # 사과 처리
+        if board[dx][dy] == 1:  # 사과일 때
+            board[dx][dy] = 0   # 사과 처리
         else:  # 꼬리 땡기기
             snake.pop()
         if move[answer] == 'D':  # 우회전
