eabc27e4ac1e29a53daba7e6ef653ac438a4d090
김준형 (JunHyung Kim) <wnsgud6232@naver.com>
2023-07-29 14:58:55 +0900

    [Bronze IV] Title: Telemarketer or not?, Time: 120 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/16017. Telemarketer or not？/Telemarketer or not？.py
@@ -0,0 +1,12 @@
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+if a < 8 or d < 8:
+    print('answer')
+else:
+    if b == c:
+        print('ignore')
+    else:
+        print('answer')
