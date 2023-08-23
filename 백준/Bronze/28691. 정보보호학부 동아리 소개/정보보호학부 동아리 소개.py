94cb2b61aa1fc1586de85b6e2d3864634b1d3e87
김준형 (JunHyung Kim) <wnsgud6232@naver.com>
2023-08-23 19:38:02 +0900

    [Bronze V] Title: 정보보호학부 동아리 소개, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/28691. 정보보호학부 동아리 소개/정보보호학부 동아리 소개.py
@@ -0,0 +1,11 @@
+x = input()
+if x == 'M':
+    print('MatKor')
+elif x == 'W':
+    print('WiCys')
+elif x == 'C':
+    print('CyKor')
+elif x == 'A':
+    print('AlKor')
+else:
+    print('$clear')
