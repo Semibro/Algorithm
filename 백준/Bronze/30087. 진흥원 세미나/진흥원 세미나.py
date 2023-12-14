367e8807415bc1514e1d2b9a72a8c8798e05552a
김준형 (JunHyung Kim) <wnsgud6232@gmail.com>
2023-12-14 20:01:44 +0900

    [Bronze V] Title: 진흥원 세미나, Time: 116 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/30087. 진흥원 세미나/진흥원 세미나.py
@@ -0,0 +1,17 @@
+N = int(input())
+for _ in range(N):
+    semina = input()
+    if semina == "Algorithm":
+        print("204")
+    elif semina == "DataAnalysis":
+        print("207")
+    elif semina == "ArtificialIntelligence":
+        print("302")
+    elif semina == "CyberSecurity":
+        print("B101")
+    elif semina == "Network":
+        print("303")
+    elif semina == "Startup":
+        print("501")
+    else:
+        print("105")
