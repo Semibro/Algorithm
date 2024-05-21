c3bcac48e9396c44d89c74aacaf1217e3cdf4080
김준형 (JunHyung Kim) <wnsgud6232@gmail.com>
2024-05-21 20:09:18 +0900

    [Silver III] Title: 타노스, Time: 116 ms, Memory: 108080 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/20310. 타노스/타노스.py
+++ 백준/Silver/20310. 타노스/타노스.py
@@ -1,3 +1,22 @@
-S = input()
+S = list(input())
 zero, one = int(S.count('0'))//2, int(S.count('1'))//2
-print('0'*zero + '1'*one)
+
+cnt = 0
+for i in S:
+    if i == '1':
+        S.remove(i)
+        cnt += 1
+        if cnt == one:
+            break
+
+cnt = 0
+S = S[::-1]
+for i in S:
+    if i == '0':
+        S.remove(i)
+        cnt += 1
+        if cnt == zero:
+            break
+
+for i in S[::-1]:
+    print(i, end="")
