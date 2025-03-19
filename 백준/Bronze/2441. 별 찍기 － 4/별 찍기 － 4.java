0408da9d13c01e55cb202813a9948d1ca9e1aadc
김준형 (JunHyung Kim) <wnsgud6232@gmail.com>
2025-03-19 10:17:41 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 104 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(br.readLine());
+        
+        for (int i = N; i > 0; i--) {
+            System.out.println(" ".repeat(N-i) + "*".repeat(i));
+        }
+    }
+}
